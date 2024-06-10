c0d7b7f2984fbf66801589d9a025f49a062b64ba
Shuchita_33 <shuchi2133@gmail.com>
2024-06-10 20:20:21 +0530

    Project setup, server creation & database connection

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+
+const app=express();
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+
+app.use(cors());
+
+const CONNECTION_URL=process.env.MONGO_URL;
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=>{
+        console.log("Database connected successfully.")
+    }))
+    .catch((error)=>console.log(error.message));
