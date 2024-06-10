cd0c25280b3404d3d628a467f2a1062740025ff7
Shuchita_33 <shuchi2133@gmail.com>
2024-06-10 22:53:21 +0530

    Client-side initial setup for posts and forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
+import {Container,AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import photo from './images/photo.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts'; 
+import useStyles from './styles';
 
 function App() {
+  const classes=useStyles();
   return (
-    <>
-    <div>
-      <h1> HII </h1>
-    </div>
-    </>
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.image} variant='h2' align='center'> Memories </Typography>
+        <img className={classes.image} src={photo} alt='imm' height="90" width='80'></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,9 @@
+import React from "react"
+const Post=()=>{
+    return(
+        <>
+        <div>This is  post</div>
+        </>
+    )
+}
+export default Post;
