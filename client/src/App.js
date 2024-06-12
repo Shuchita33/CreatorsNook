10aff62d681c3ccf909a81e79e5ed7c1c0d62e54
Shuchita_33 <shuchi2133@gmail.com>
2024-06-12 12:53:22 +0530

    Add routing to App.js using react router, by creating Navbar, Home components, with signup link to Auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,25 @@
 import './App.css';
-import {Container,AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import photo from './images/photo.png';
-import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts'; 
-import useStyles from './styles';
+import {Container} from '@material-ui/core';
 
-import { useDispatch } from 'react-redux';
-import {useState, useEffect } from 'react';
-
-import getPosts from './actions/posts';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 function App() {
-  const[currentId,setCurrentId]=useState(null);
-  const classes=useStyles();
-  const dispatch=useDispatch();
-
-  useEffect(()=>{
-    dispatch(getPosts());
-  },[currentId,dispatch]) // to reload the app on the currentId change also (upon updation)
-
+  
   return (
+    <BrowserRouter>
     <Container maxWidth='lg'>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.image} variant='h2' align='center'> CreatorsNook </Typography>
-        <img className={classes.image} src={photo} alt='imm' height="90" width='80'></img>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3} >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId}/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' Component={Home}></Route>
+        <Route path='/auth' Component={Auth}></Route>
+      </Routes>
+      
     </Container>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import { useDispatch } from "react-redux";
+import {useState, useEffect } from 'react';
+import getPosts from '../../actions/posts';
+import {Container, Grow, Grid} from '@material-ui/core';
+import useStyles from '../../styles';
+
+const Home=()=>{
+    const[currentId,setCurrentId]=useState(null);
+    const classes=useStyles();
+    const dispatch=useDispatch();
+
+    useEffect(()=>{
+        dispatch(getPosts());
+    },[currentId,dispatch]) // to reload the app on the currentId change also (upon updation)
+
+
+    return(
+        <Grow in>
+        <Container>
+          <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3} >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId}/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    )
+}
+export default Home;
